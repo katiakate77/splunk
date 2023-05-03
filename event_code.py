c16ce6afe3a8e8f3d950ef50eeeebe51ca458c8d
katiakate <ekaterina@imac.local>
2023-05-03 16:21:55 +0300

    class based realization

--- event_code.py
+++ event_code.py
@@ -12,115 +12,130 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 load_dotenv()
 
-SCHEME = os.getenv('SCHEME')
-HOST = os.getenv('HOST')
-PORT = os.getenv('PORT')
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
+
+class SplunkBase:
+    SCHEME = os.getenv('SCHEME')
+    HOST = os.getenv('HOST')
+    PORT = os.getenv('PORT')
+    USERNAME = os.getenv('USERNAME')
+    PASSWORD = os.getenv('PASSWORD')
+
+    @classmethod
+    def get_base_url(cls):
+        return '{}://{}:{}/servicesNS/{}/'.format(
+            cls.SCHEME, cls.HOST, cls.PORT, cls.USERNAME
+            )
 
 
-def get_base_url():
-    return '{}://{}:{}/servicesNS/{}/'.format(SCHEME, HOST, PORT, USERNAME)
+class SplunkAuth(SplunkBase):
+    @classmethod
+    def get_auth_url(cls):
+        app = 'system'
+        return '{}{}/auth/login/'.format(cls.get_base_url(), app)
+
+    @classmethod
+    def get_api_auth_answer(cls):
+        with requests.Session() as session:
+            session.verify = False
+            payload = {
+                'username': cls.USERNAME,
+                'password': cls.PASSWORD,
+                'output_mode': 'json',
+            }
+            try:
+                response = session.post(cls.get_auth_url(), data=payload)
+                if response.ok:
+                    logging.info(f'Получили ответ от API {cls.get_auth_url()}')
+                else:
+                    logging.error(f'Не удалось получить ответ от API, '
+                                  f'код ошибки {response.status_code}')
+                    raise Exception(response.status_code)
+            except Exception as e:
+                logging.error(f'Недоступность эндпоинта, ошибка: {e}')
+                raise Exception('Недоступность эндпоинта')
+            return session, response.json()
+
+    @classmethod
+    def parse_session_key(cls, resp):
+        try:
+            sess_key = resp.get('sessionKey')
+            logging.info('Получили `sessionKey`')
+        except KeyError:
+            logging.error('Отсутствие `sessionKey` в ответе API')
+            raise KeyError('Нет ожидаемого ключа')
+        headers = {
+            'Authorization': 'Splunk {}'.format(sess_key),
+        }
+        return headers
+
+    @classmethod
+    def set_headers(cls, auth_sess, headers):
+        auth_sess.headers.update(headers)
+        logging.info('Установлены заголовки для всей сессии')
 
 
-def get_auth_url():
-    app = 'system'
-    return '{}{}/auth/login/'.format(get_base_url(), app)
+# class SplunkSearch(SplunkAuth):
+#     CURRENT_SESSION = SplunkAuth.set_headers()
+
+#     @classmethod
+#     def get_search_url(cls):
+#         app = 'search'
+#         return '{}{}/search/jobs/'.format(cls.get_base_url(), app)
+
+#     def search_request(self, search_query):
+#         payload = {
+#             'search': search_query,
+#             'output_mode': 'json',
+#         }
+#         try:
+#             response = self.CURRENT_SESSION.post(
+#                 self.get_search_url(), data=payload
+#                 )
+#             if response.ok:
+#                 logging.info(f'Получили ответ от API {self.get_search_url()}')
+#             else:
+#                 logging.error(f'Не удалось получить ответ от API, '
+#                               f'код ошибки {response.status_code}')
+#                 raise Exception(response.status_code)
+#         except Exception as e:
+#             logging.error(f'Недоступность эндпоинта, ошибка: {e}')
+#             raise Exception('Недоступность эндпоинта')
+#         return response.json()
 
 
-def get_api_auth_answer(sess):
-    auth_url = get_auth_url()
-    payload = {
-        'username': USERNAME,
-        'password': PASSWORD,
-        'output_mode': 'json',
-    }
-    try:
-        response = sess.post(auth_url, data=payload)
-        if response.ok:
-            logging.info(f'Получили ответ от API {auth_url}')
-        else:
-            logging.error(f'Не удалось получить ответ от API, '
-                          f'код ошибки {response.status_code}')
-            raise Exception(response.status_code)
-    except Exception as e:
-        logging.error(f'Недоступность эндпоинта, ошибка: {e}')
-        raise Exception('Недоступность эндпоинта')
-    return response.json()
+# def parse_sid(resp):
+#     try:
+#         sid = resp.get('sid')
+#         logging.info(f'Получили `sid`: {sid}')
+#     except KeyError:
+#         logging.error('Отсутствие `sid` в ответе API')
+#         raise KeyError('Нет ожидаемого ключа')
+#     return sid
 
 
-def parse_session_key(response):
-    try:
-        sess_key = response.get('sessionKey')
-        logging.info('Получили `sessionKey`')
-    except KeyError:
-        logging.error('Отсутствие `sessionKey` в ответе API')
-        raise KeyError('Нет ожидаемого ключа')
-    headers = {
-        'Authorization': 'Splunk {}'.format(sess_key),
-    }
-    return headers
+# def get_job_status_url(sid):
+#     return get_search_url() + '{}'.format(sid)
 
 
-def get_search_url():
-    app = 'search'
-    return '{}{}/search/jobs/'.format(get_base_url(), app)
-
-
-def search_request(sess, search_query):
-    search_url = get_search_url()
-    payload = {
-        'search': search_query,
-        'output_mode': 'json',
-    }
-    try:
-        response = sess.post(search_url, data=payload)
-        if response.ok:
-            logging.info(f'Получили ответ от API {search_url}')
-        else:
-            logging.error(f'Не удалось получить ответ от API, '
-                          f'код ошибки {response.status_code}')
-            raise Exception(response.status_code)
-    except Exception as e:
-        logging.error(f'Недоступность эндпоинта, ошибка: {e}')
-        raise Exception('Недоступность эндпоинта')
-    return response.json()
-
-
-def parse_sid(resp):
-    try:
-        sid = resp.get('sid')
-        logging.info(f'Получили `sid`: {sid}')
-    except KeyError:
-        logging.error('Отсутствие `sid` в ответе API')
-        raise KeyError('Нет ожидаемого ключа')
-    return sid
-
-
-def get_job_status_url(sid):
-    return get_search_url() + '{}'.format(sid)
-
-
-def get_result_url(sid):
-    return get_search_url() + '{}/results'.format(sid)
+# def get_result_url(sid):
+#     return get_search_url() + '{}/results'.format(sid)
 
 
 def main():
-    """Основная логика работы бота."""
     logging.basicConfig(
         level=logging.DEBUG,
         filename='log.log',
         format='%(asctime)s, %(levelname)s, %(message)s',
     )
-    with requests.Session() as session:
-        session.verify = False
-        response = get_api_auth_answer(session)
-        headers = parse_session_key(response)
-        session.headers.update(headers)
-        logging.info('Установлены заголовки для всей сессии')
-        search_query_resp = search_request(session, SEARCH_QUERY_1)
-        sid = parse_sid(search_query_resp)
-        print(get_job_status_url(sid))
+    session_, resp_ = SplunkAuth.get_api_auth_answer()
+    headers_ = SplunkAuth.parse_session_key(resp_)
+    SplunkAuth.set_headers(session_, headers_)
+    # obj_1 = SplunkSearch()
+    # print(obj_1)
+    # print(obj_1.search_request(search_query=SEARCH_QUERY_1))
+    # search_query_resp = search_request(session, SEARCH_QUERY_1)
+    # sid = parse_sid(search_query_resp)
+    # print(get_job_status_url(sid))
 
 
 if __name__ == '__main__':
