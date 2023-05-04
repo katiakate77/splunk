23bbd5674fb743160806854677e6a890b86b22d6
katiakate <ekaterina@imac.local>
2023-05-04 14:42:16 +0300

    auth done

--- event_code.py
+++ event_code.py
@@ -12,6 +12,12 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 load_dotenv()
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='log.log',
+    format='%(asctime)s, %(levelname)s, %(message)s',
+)
+
 
 class SplunkBase:
     SCHEME = os.getenv('SCHEME')
@@ -35,6 +41,7 @@ class SplunkAuth(SplunkBase):
 
     @classmethod
     def get_api_auth_answer(cls):
+        auth_url = cls.get_auth_url()
         with requests.Session() as session:
             session.verify = False
             payload = {
@@ -43,9 +50,9 @@ class SplunkAuth(SplunkBase):
                 'output_mode': 'json',
             }
             try:
-                response = session.post(cls.get_auth_url(), data=payload)
+                response = session.post(auth_url, data=payload)
                 if response.ok:
-                    logging.info(f'Получили ответ от API {cls.get_auth_url()}')
+                    logging.info(f'Получили ответ от API {auth_url}')
                 else:
                     logging.error(f'Не удалось получить ответ от API, '
                                   f'код ошибки {response.status_code}')
@@ -68,17 +75,6 @@ class SplunkAuth(SplunkBase):
         }
         return headers
 
-    # @classmethod
-    # def set_headers(cls):
-    #     session_, resp_ = cls.get_api_auth_answer()
-    #     headers_ = cls.parse_session_key(resp_)
-    #     session_.headers.update(headers_)
-    #     logging.info('Установлены заголовки для всей сессии')
-    #     return session_
-
-
-class SplunkSearch(SplunkAuth):
-    # CURRENT_SESSION = SplunkAuth.set_headers()
     @classmethod
     def set_headers(cls):
         session_, resp_ = cls.get_api_auth_answer()
@@ -87,22 +83,27 @@ class SplunkSearch(SplunkAuth):
         logging.info('Установлены заголовки для всей сессии')
         return session_
 
+
+class SplunkSearch(SplunkAuth):
+    CURRENT_SESSION = SplunkAuth.set_headers()
+
     @classmethod
     def get_search_url(cls):
         app = 'search'
         return '{}{}/search/jobs/'.format(cls.get_base_url(), app)
 
     def search_request(self, search_query):
+        search_url = self.get_search_url()
         payload = {
             'search': search_query,
             'output_mode': 'json',
         }
         try:
-            response = self.set_headers().post(
-                self.get_search_url(), data=payload
+            response = self.CURRENT_SESSION.post(
+                search_url, data=payload
                 )
             if response.ok:
-                logging.info(f'Получили ответ от API {self.get_search_url()}')
+                logging.info(f'Получили ответ от API {search_url}')
             else:
                 logging.error(f'Не удалось получить ответ от API, '
                               f'код ошибки {response.status_code}')
@@ -121,24 +122,33 @@ class SplunkSearch(SplunkAuth):
             raise KeyError('Нет ожидаемого ключа')
         return sid
 
+    @classmethod
+    def get_job_status_url(cls, sid):
+        return cls.get_search_url() + '{}'.format(sid)
 
-# def get_job_status_url(sid):
-#     return get_search_url() + '{}'.format(sid)
+    def get_job_status(self, sid_):
+        payload = {
+            'output_mode': 'json',
+        }
+        job_status_query = self.CURRENT_SESSION.get(
+            self.get_job_status_url(sid_), data=payload
+            )
+        return job_status_query
 
+#     def is_done(self):
+#         pass
 
-# def get_result_url(sid):
-#     return get_search_url() + '{}/results'.format(sid)
+#     @classmethod
+#     def get_result_url(cls, sid):
+#         return cls.get_search_url() + '{}/results'.format(sid)
 
 
 def main():
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filename='log.log',
-        format='%(asctime)s, %(levelname)s, %(message)s',
-    )
-    obj_1 = SplunkSearch()
-    r_1 = obj_1.search_request(search_query=SEARCH_QUERY_1)
-    obj_1.parse_sid(r_1)
+    job_1 = SplunkSearch()
+    resp_1 = job_1.search_request(search_query=SEARCH_QUERY_1)
+    sid_1 = job_1.parse_sid(resp_1)
+    status_1 = job_1.get_job_status(sid_1)
+    print(status_1)
 
 
 if __name__ == '__main__':
